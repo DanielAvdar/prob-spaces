5d72060d3ca702b1daa559c925425300f0f52ab4
DanielAvdar <66269169+DanielAvdar@users.noreply.github.com>
2025-03-27 20:36:52 +0200

    Add support for masks in `test_multidiscrete_initialization`
    Updated the test to include mask parameterization, enabling tests with various masking configurations. Adjustments were made to handle masks in probability distribution initialization and ensure proper validation of log probabilities.
    
    Signed-off-by: DanielAvdar <66269169+DanielAvdar@users.noreply.github.com>

--- tests/test_discrete.py
+++ tests/test_discrete.py
@@ -33,24 +33,96 @@ def test_discrete_initialization(n, start, probs, device):
     [
         (
             [2, 3, 4],
-            [[[0.1, 0.1, 0.4, 0.2, 0.2], [0.1, 0.1, 0.4, 0.2, 0.2], [0.1, 0.1, 0.4, 0.2, 0.2]]],
-        ),  # Example with incorrect probs size
-        # ([2, 3, 4], [[0.5, 0.5], [0.3, 0.3, 0.4], [0.2, 0.2, 0.3, 0.3]]),  # Correct probs size
-        # ([3, 2,6], [[0.3, 0.3, 0.4], [0.6, 0.4]]),  # Another correct probs size
+            [
+                [
+                    [0.1, 0.1, 0.4, 0.2, 0.2],
+                    [0.1, 0.1, 0.4, 0.2, 0.2],
+                    [0.1, 0.1, 0.4, 0.2, 0.2],
+                ]
+            ],
+        ),
+        (
+            [3, 2, 4],
+            [
+                [
+                    [0.1, 0.1, 0.4, 0.2, 0.2],
+                    [0.1, 0.1, 0.4, 0.2, 0.2],
+                    [0.1, 0.1, 0.4, 0.2, 0.2],
+                ]
+            ],
+        ),
+        (
+            [4, 4, 4],
+            [
+                [
+                    [0.1, 0.1, 0.4, 0.2, 0.2],
+                    [0.1, 0.1, 0.4, 0.2, 0.2],
+                    [0.1, 0.1, 0.4, 0.2, 0.2],
+                ]
+            ],
+        ),
+        (
+            [4, 2, 2],
+            [
+                [
+                    [0.1, 0.1, 0.4, 0.2, 0.2],
+                    [0.1, 0.1, 0.4, 0.2, 0.2],
+                    [0.1, 0.1, 0.4, 0.2, 0.2],
+                ]
+            ],
+        ),
     ],
 )
 @pytest.mark.parametrize(
     "start",
     [[0, 1, -1], [-3, 0, -0]],
 )
-def test_multidiscrete_initialization(nvec, start, probs, device):
+@pytest.mark.parametrize(
+    "mask",
+    [
+        None,
+        [
+            [True, False, True, False, True],
+            [True, False, True, False, True],
+            [True, False, True, False, True],
+        ],
+        [
+            [False, True, False, True, False],
+            [False, True, False, True, False],
+            [False, True, False, True, False],
+        ],
+        [
+            [True, True, True, True, True],
+            [True, True, True, True, True],
+            [True, True, True, True, True],
+        ],
+        [
+            [True, True, False, False, False],
+            [True, True, False, False, False],
+            [True, True, False, False, False],
+        ],
+        [
+            [True, False, False, False, False],
+            [True, False, False, False, False],
+            [True, False, False, False, False],
+        ],
+    ],
+)
+def test_multidiscrete_initialization(nvec, start, probs, mask, device):
     multi_discrete = MultiDiscreteDist(nvec=nvec, start=start)
-    prob_dist = multi_discrete(prob=th.tensor(probs, dtype=th.float32, device=device))
+    prob_dist = multi_discrete(
+        prob=th.tensor(probs, dtype=th.float32, device=device, requires_grad=True),
+        mask=th.tensor(mask, dtype=th.bool, device=device) if mask is not None else None,
+    )
     sample = prob_dist.sample()
     sample_list = sample.cpu().numpy().tolist()
     assert len(sample_list) == len(nvec)
     diffs = np.abs(multi_discrete.nvec)
     assert np.sum(diffs) == np.sum(multi_discrete.internal_mask)
-    prob_dist.log_prob(sample)
+    log_probs = prob_dist.log_prob(sample)
     # assert all([multi_discrete.contains(s) for s in sample_list])
     assert multi_discrete.contains(sample.cpu().numpy())
+    assert not th.any(th.isinf(log_probs))
+    assert not th.any(th.isneginf(log_probs))
+    assert not th.any(th.isnan(log_probs))
+    assert log_probs.requires_grad
